5a868994044e055d7e98003ebc11b775cb631fe3
drago1979 <78018887+drago1979@users.noreply.github.com>
2022-04-12 12:18:04 +0200

    WIP

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends Controller
      */
     public function login(Request $request): JsonResponse
     {
+        
         $content = json_decode($request->getContent());
 
         $login = $content->login;
